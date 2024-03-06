aca781fb65ba6bfadee29f030018192a97e604d1
james <jleo56291@gmail.com>
2024-03-06 20:37:23 +0300

    Completed Navbar

---
+++ ticketing/src/Components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">
+          <img src="/logo.png" alt="Logo" />
+        </Link>
+      </div>
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/events">Events</Link></li>
+        <li><Link to="/testimonial">Testimonial</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/signup">Signup</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
