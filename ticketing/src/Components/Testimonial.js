dd10d800499ee4af59b6a743e0ce67c377ea73d0
james <jleo56291@gmail.com>
2024-03-07 20:28:48 +0300

    Testimonial

--- ticketing/src/Components/Testimonial.js
+++ ticketing/src/Components/Testimonial.js
@@ -1,11 +1,68 @@
-import React from 'react'
+// Testimonial.js
+
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const Testimonial = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  };
+
+  const data = [
+    {
+      name: `James Kin`,
+      img: `/images/John_Morgan.jpg`,
+      review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+    },
+    {
+      name: `Ellie Mike`,
+      img: `/students/Ellie_Anderson.jpg`,
+      review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+    },
+    {
+      name: `Maurine Wam`,
+      img: `/images/Nia_Adebayo.jpg`,
+      review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+    },
+    {
+      name: `Rigo luis`,
+      img: `/images/Rigo_Louie.jpg`,
+      review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+    },
+    {
+      name: `Diko Milliams`,
+      img: `/images/Mia_Williams.jpg`,
+      review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+    },
+  ];
+
   return (
-    <div>
-      Testimonial
+    <div className='w-3/4 m-auto'>
+      <div className="mt-20">
+        <Slider {...settings}>
+          {data.map((d) => (
+            <div key={d.name} className="bg-white h-[450px] text-black rounded-xl">
+              <div className='h-56 bg-indigo-500 flex justify-center items-center rounded-t-xl'>
+                <img src={d.img} alt="" className="h-44 w-44 rounded-full"/>
+              </div>
+
+              <div className="flex flex-col items-center justify-center gap-4 p-4">
+                <p className="text-xl font-semibold">{d.name}</p>
+                <p className="text-center">{d.review}</p>
+                <button className='bg-indigo-500 text-white text-lg px-6 py-1 rounded-xl'>Read More</button>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Testimonial;
