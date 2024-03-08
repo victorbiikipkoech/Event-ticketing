596785a9f88b2c146cf79a904cb34e4dd3471c30
james <jleo56291@gmail.com>
2024-03-08 17:28:01 +0300

    completed Homepage

--- ticketing/src/Components/Events.js
+++ ticketing/src/Components/Events.js
@@ -1,11 +1,59 @@
-import React from 'react'
+import React from 'react';
+import EventImage1 from '../images/colorfest.jpg';
+import EventImage2 from '../images/halloween.jpg';
+import EventImage3 from '../images/food.jpg';
+import EventImage4 from '../images/music.jpg';
+
+const eventData = [
+  {
+    id: 1,
+    name: 'Event 1',
+    description: 'Description for Event 1',
+    date: '2024-03-10',
+    image: EventImage1,
+  },
+  {
+    id: 2,
+    name: 'Event 2',
+    description: 'Description for Event 2',
+    date: '2024-04-15',
+    image: EventImage2,
+  },
+  {
+    id: 3,
+    name: 'Event 3',
+    description: 'Description for Event 3',
+    date: '2024-05-10',
+    image: EventImage3,
+  },
+  {
+    id: 4,
+    name: 'Event 4',
+    description: 'Description for Event 4',
+    date: '2024-06-10',
+    image: EventImage4,
+  },
+];
 
 const Events = () => {
   return (
-    <div>
-      Events
+    <div className="container mx-auto flex flex-wrap justify-center gap-4 mt-20"> {/* Added mt-20 for top margin */}
+      {eventData.map((event) => (
+        <div key={event.id} className="max-w-sm rounded overflow-hidden shadow-lg">
+          <img className="w-full" src={event.image} alt={event.name} />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2 text-white">{event.name}</div> {/* Changed text-gray-700 to text-white */}
+            <p className="text-white text-base"> {/* Changed text-gray-700 to text-white */}
+              {event.description}
+            </p>
+          </div>
+          <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{event.date}</span>
+          </div>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Events
+export default Events;
--- ticketing/src/Components/Home.js
+++ ticketing/src/Components/Home.js
@@ -5,20 +5,23 @@ const Home = () => {
   return (
     <div>
      <h1 className='home'>Welcome to the Home Page</h1>
-     <div>
-      
-     <img className='w-full h-screen overflow-hidden'
-      src={backgroundImage}
-      
-      alt="Home Page Image" />
+
+     <div className='absolute top-0 left-0 z-0 w-full h-screen overflow-hidden'>
+         <img className='w-4/5 h-screen mx-auto overflow-hidden' 
+         src={backgroundImage} alt="Home Page Image" />
      </div>
+
      
-      <div className='text-bold'>
-      <p className=''>Unlock unforgettable experiences with just a ticket. Your gateway to excitement awaits!</p>
+     <div className='relative z-10 text-white text-center mt-24'>
+  <p className='text-4xl font-extrabold leading-none text-transparent bg-clip-text bg-gradient-to-br from-white to-black'>
+    Unlock unforgettable experiences<br/>with just a ticket<br/>Your gateway to excitement<br/>awaits!
+  </p>
+</div>
+
+
+
 
       </div>
-
-    </div>
   );
 };
 
