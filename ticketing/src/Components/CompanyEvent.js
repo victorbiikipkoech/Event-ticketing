427d0d2427b068ee6e9f99c93571785595dc5d12
james <jleo56291@gmail.com>
2024-03-17 22:40:37 +0300

    completed new event

--- ticketing/src/Components/CompanyEvent.js
+++ ticketing/src/Components/CompanyEvent.js
@@ -55,86 +55,94 @@ function CompanyEvent() {
   };
 
   return (
-    <div>
-      <h1>New Event</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Venue Name</label>
+    <div className="container mx-auto">
+      <h1 className="text-2xl font-bold text-center mb-4 mr-96">New Event</h1>
+      <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-x-4">
+        <div className="mb-4">
+          <label className="block">Venue Name</label>
           <input
             type='text'
             placeholder='Venue Name'
             value={venue_name}
             onChange={e => setVenueName(e.target.value)}
+            className="input"
           />
         </div>
-        <div>
-          <label>Location</label>
+        <div className="mb-4">
+          <label className="block">Location</label>
           <input
             type='text'
             placeholder='Event Location'
             value={location}
             onChange={e => setLocation(e.target.value)}
+            className="input"
           />
         </div>
-        <div>
-          <label>Event Name</label>
+        <div className="mb-4">
+          <label className="block">Event Name</label>
           <input
             type='text'
             placeholder='Event Name'
             value={event_name}
             onChange={e => setEventName(e.target.value)}
+            className="input"
           />
         </div>
-        <div>
-          <label>Start Date</label>
+        <div className="mb-4">
+          <label className="block">Start Date</label>
           <input
             type='date'
             placeholder='Start Date'
             value={start_date}
             onChange={e => setStartDate(e.target.value)}
+            className="input"
           />
         </div>
-        <div>
-          <label>End Date</label>
+        <div className="mb-4">
+          <label className="block">End Date</label>
           <input
             type='date'
             placeholder='End Date'
             value={end_date}
             onChange={e => setEndDate(e.target.value)}
+            className="input"
           />
         </div>
-        <div>
-          <label>Event Time</label>
+        <div className="mb-4">
+          <label className="block">Event Time</label>
           <input
             type='time'
             placeholder='Event Time'
             value={event_time}
             onChange={e => setEventTime(e.target.value)}
+            className="input"
           />
         </div>
-        <div>
-          <label>Event Description</label>
+        <div className="mb-4">
+          <label className="block">Event Description</label>
           <textarea
             placeholder='Description of the event'
             value={description}
             onChange={e => setDescription(e.target.value)}
             required
+            className="input"
           />
         </div>
-        <div>
-          <label>Event Type</label>
+        <div className="mb-4">
+          <label className="block">Event Type</label>
           <select
             value={event_type}
             onChange={e => setEventType(e.target.value)}
+            className="input"
           >
             <option>Select Event Type</option>
-            <option>Physical</option>
-            <option>Hybrid</option>
-            <option>Remote</option>
+            <option>VVIP</option>
+            <option>VIP</option>
+            <option>REGULAR</option>
           </select>
         </div>
-        <div>
-          <button type='submit'>Create Event</button>
+        <div className="col-span-2 flex justify-center">
+          <button type='submit' className="btn bg-blue-500 text-white px-4 py-2 rounded mr-96 hover:bg-blue-600">Create Event</button>
         </div>
       </form>
     </div>
